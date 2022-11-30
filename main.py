d85160f61143e990a96e7b7ecbfa46f27a6acf49
Yuy-1 <DM-Yuy@proton.me>
2022-11-30 06:09:54 -0500

    import body from fastapi.params setup create_post function for postman testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -7,6 +8,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/post")
 def get_post():
     return {'data': 'This is your posts'}
+
+
+@app.post("/createposts")
+def create_post(payLoad: dict = Body(...)):
+    print(payLoad)
+    return{"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+
