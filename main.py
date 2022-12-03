7eabb5439c7276520d541867fd7838089d6c13a5
Yuy-1 <DM-Yuy@proton.me>
2022-12-03 16:07:26 -0500

    add find_index_post to return post in an array add delete_post to delete requested post and return 204 status code

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ def find_post(id):
             return p
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -49,3 +55,10 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} was not found")
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    my_posts.pop(index)
+    return {'message': 'post was successfully deleted'}
