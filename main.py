758f6528e461da1a2f06c1db1eadfe2369133b25
Yuy-1 <DM-Yuy@proton.me>
2022-12-03 15:45:40 -0500

    import status, HTTPException add 404 error code when requesting a post ID that does not exist in postman inside get_post function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -15,10 +16,12 @@ class Post(BaseModel):
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
+
 def find_post(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+
 
 @app.get("/")
 async def root():
@@ -35,11 +38,14 @@ def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
-    return{"data": post_dict}
+    return {"data": post_dict}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):
     post = find_post(id)
     print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} was not found")
     return {"post_detail": post}
